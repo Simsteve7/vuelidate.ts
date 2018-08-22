4c8c92d4fa168a010ab0085a66cb062eab2dcdce
Steve Maris <steve_maris@hotmail.com>
2018-08-22 17:23:19 +0200

    Remove .idea/

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,59 @@
-/// <reference path="vuelidate.d.ts" />
-// TypeScript Version: 2.3
 import Vue from 'vue'
-import { Validation, ValidationRule } from 'vuelidate'
+
+declare module 'vuelidate' {
+
+    /**
+     * Covers beforeCreate(), beforeDestroy() and data().
+     *
+     * No public members.
+     */
+    export const validationMixin: {
+        data(): any
+        beforeCreate(): void
+        beforeDestroy(): void
+    }
+
+    export interface Validation extends Vue {
+       // const validationGetters
+       readonly $invalid: boolean
+       readonly $dirty: boolean
+       readonly $error: boolean
+       readonly $pending: boolean
+       readonly $params: boolean
+
+       // const validationMethods
+       $touch(): never
+       $reset(): never
+       $flattenparams(): Params[]
+    }
+
+    export interface ValidationPredicate {}
+
+    // const ValidationRule
+    export interface ValidationRule {
+        $params(): Params
+        $pending(): boolean
+    }
+
+    // pre-defined rules
+    export const required: ValidationRule
+    export const requiredIf: (field: string) => ValidationRule
+    export const requiredUnless: (field: string) => ValidationRule
+    export const sameAs: (field: string) => ValidationRule
+
+    // build your own rule
+    export const withParams: (params: Params, rule: CustomRule) => ValidationRule
+    export interface Params {
+        type: string
+    }
+    export interface CustomRule extends ValidationRule {
+        (value: any, parentVm?: Vue): boolean
+    }
+}
 
 declare module 'vue/types/vue' {
     interface Vue {
-        $v: { [attr: string]: Validation }
+        $v: { [attr: string]: any }
     }
 }
 
---
+++ lib/validators/index.d.ts
@@ -0,0 +1,58 @@
+declare module "vuelidate/lib/validators" {
+    import {ValidationPredicate} from "vuelidate";
+
+    /**
+     * Accepts only alphabet characters.
+     */
+    export function alpha(value: any): boolean;
+
+    /**
+     * Accepts only alphanumerics.
+     */
+    export function alphaNum(value: any): boolean;
+
+    /**
+     * Accepts only numeric.
+     */
+    export function numeric(numeric: any): boolean;
+
+    /**
+     * Checks if a number is in specified bounds. Min and max are both inclusive.
+     */
+    export function between(min: number, max: number): (value: any) => boolean;
+
+    /**
+     * Accepts valid email addresses. Keep in mind you still have to carefully verify it on your server, as it is impossible to tell if the address is real without sending verification email.
+     */
+    export function email(value: any): boolean;
+
+    /**
+     * Requires the input to have a maximum specified length, inclusive. Works with arrays.
+     */
+    export function maxLength(max: number): (value: any) => boolean;
+
+    /**
+     * Requires the input to have a minimum specified length, inclusive. Works with arrays.
+     */
+    export function minLength(min: number): (value: any) => boolean;
+
+    /**
+     * Requires non-empty data. Checks for empty arrays and strings containing only whitespaces.
+     */
+    export function required(value: any): boolean;
+
+    /**
+     * Checks for equality with a given property. Locator might be either a sibling property name or a function, that will get your component as this and nested model which sibling properties under second parameter.
+     */
+    export function sameAs(locator: string): (value: any, vm?: any) => boolean;
+
+    /**
+     * Passes when at least one of provided validators passes.
+     */
+    export function or(...validators: ValidationPredicate[]): () => boolean;
+
+    /**
+     * Passes when all of provided validators passes.
+     */
+    export function and(...validators: ValidationPredicate[]): () => boolean;
+}
